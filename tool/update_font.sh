a7f013adc2f1198f742379cbd56f6ca2b60b3f0f
Jordy de Jonghe <jordy.dejonghe@icapps.com>
2025-05-13 16:35:08 +0200

    added previews

--- tool/update_font.sh
+++ tool/update_font.sh
@@ -5,12 +5,27 @@ then
   cd ..
 fi
 
+echo "--- Reorienting icons... ---"
 npx reorient icons/*.svg
+
+echo "--- Removing old previews... ---"
+rm -rf ./previews/*
+
+echo "--- Creating new previews... ---"
+magick mogrify -format png -resize 24x24 -path ./previews icons/*.svg
+
+echo "--- Removing old font... ---"
 rm -rf lib/fonts/icappsIcons.ttf
+
+echo "--- Creating new font... ---"
 npx fantasticon icons --output ./lib/fonts -t ttf --name icappsIcons -g ts
 rm -rf lib/fonts/icappsIcons.ts
 cd lib/fonts
+
+echo "--- Generating icappsIcons.dart... ---"
 ttx icappsIcons.ttf
 cd ../..
 dart run icon_parser/lib/icon_parser.dart
+
+echo "--- Cleanup ... ---"
 rm -rf lib/fonts/icappsIcons.ttx
